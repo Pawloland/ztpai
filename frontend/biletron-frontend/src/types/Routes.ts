8b990955e78f8ed1ae9288f6adc7c1ba845c407e
Pawloland <59684145+Pawloland@users.noreply.github.com>
2025-04-28 14:21:07 +0200

    feat(react): Set up client routing

---
+++ frontend/biletron-frontend/src/types/Routes.ts
@@ -0,0 +1,10 @@
+export enum AllowedRoutes {
+    Home = '/',
+    Login = '/login',
+    Register = '/register',
+    Logout = '/logout',
+    WorkerLogin = '/workerLogin',
+    Dashboard = '/dashboard',
+    WorkerLogout = '/workerLogout',
+    Hamburger = '#',
+}
